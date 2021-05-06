7eea5a936c4523d30bf8b4013b6983c8d163eef8
Eli Villa <evilla@mit.edu>
2021-05-06 17:58:39 -0400

    Dungeon rom

--- Eurydice/Assets/Scripts/PaintingScript.cs
+++ Eurydice/Assets/Scripts/PaintingScript.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PaintingScript : MonoBehaviour
 {
     private Material paintingMaterial;
+    public VideoPlayer videoPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        paintingMaterial = GetComponent<Renderer>().material;
-        paintingMaterial.color = Color.red;
+        //paintingMaterial = GetComponent<Renderer>().material;
+        //paintingMaterial.color = Color.red;
     }
 
     public void ActivatePainting() {
-        paintingMaterial.color = Color.green;
+        //paintingMaterial.color = Color.green;
+        videoPlayer.Play();
     }
 
     public void DeactivatePainting() {
-        paintingMaterial.color = Color.red;
+        //paintingMaterial.color = Color.red;
     }
 }
