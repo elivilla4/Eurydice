3686fc8885d7120c99cf56384a3ef1127f3d8856
Daniel Adebi <ikadebi@mit.edu>
2021-05-20 20:36:51 -0400

    Finished Orpheus movement

--- Eurydice/Assets/Scripts/ContinuousMovement.cs
+++ Eurydice/Assets/Scripts/ContinuousMovement.cs
@@ -7,6 +7,7 @@ public class ContinuousMovement : MonoBehaviour
 {
     public float speed = 1.0f;
     public XRNode inputSource;
+    public GameObject orpheus;
 
     private OVRCameraRig rig;
     private Vector2 inputAxis;
@@ -14,11 +15,21 @@ public class ContinuousMovement : MonoBehaviour
 
     int currentPosition = 0;
 
+    // Orpheus checkpoints
+    Vector3 orphCkpt1 = new Vector3(2.3f, 2.3f, -24);
+    Vector3 orphCkpt2 = new Vector3(-6f, 2.3f, -24);
+    Vector3 orphCkpt3 = new Vector3(-6f, 2.3f, -32);
+    Vector3 orphCkpt4 = new Vector3(2.3f, 2.3f, -32);
+    Vector3 orphCkpt5 = new Vector3(-3.3f, 2.3f, -20);
+
+    float orpheusSpeed = 1.2f;
+
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
         rig = GetComponentInChildren<OVRCameraRig>();
+        orpheus.gameObject.transform.position = new Vector3(2.3f, 2.3f, -16);
     }
 
     // Update is called once per frame
@@ -30,13 +41,37 @@ public class ContinuousMovement : MonoBehaviour
         Quaternion headYaw = Quaternion.Euler(0, rig.gameObject.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
         character.Move(direction * Time.deltaTime * speed);
+        if (currentPosition == 1 && Vector3.Distance(orpheus.transform.position, orphCkpt1) > 0.001f)
+        {
+            orpheus.transform.position = Vector3.MoveTowards(orpheus.transform.position, orphCkpt1, orpheusSpeed * Time.deltaTime);
+        } 
+        else if (currentPosition == 2 && Vector3.Distance(orpheus.transform.position, orphCkpt2) > 0.001f)
+        {
+            orpheus.transform.position = Vector3.MoveTowards(orpheus.transform.position, orphCkpt2, orpheusSpeed * Time.deltaTime);
+        }
+        else if (currentPosition == 3 && Vector3.Distance(orpheus.transform.position, orphCkpt3) > 0.001f)
+        {
+            orpheus.transform.position = Vector3.MoveTowards(orpheus.transform.position, orphCkpt3, orpheusSpeed * Time.deltaTime);
+        }
+        else if (currentPosition == 4 && Vector3.Distance(orpheus.transform.position, orphCkpt4) > 0.001f)
+        {
+            orpheus.transform.position = Vector3.MoveTowards(orpheus.transform.position, orphCkpt4, orpheusSpeed * Time.deltaTime);
+        }
+        else if (currentPosition == 5 && Vector3.Distance(orpheus.transform.position, orphCkpt5) > 0.001f)
+        {
+            orpheus.transform.position = Vector3.MoveTowards(orpheus.transform.position, orphCkpt5, orpheusSpeed * Time.deltaTime);
+        }
     }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals("checkpoint1") && currentPosition == 0)
         {
+            print("checkpoint 1");
             currentPosition += 1;
+            
         }
         else if (other.gameObject.name.Equals("checkpoint2") && currentPosition == 1)
         {
